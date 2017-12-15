bc4d211e277fd326c6e6cbe26fd5a7cda62c27ed
wjwka <819365420@qq.com>
2017-12-15 12:46:59 +0100

    updata from canvas

--- practical_data_analysis/evaluate.py
+++ practical_data_analysis/evaluate.py
@@ -1,2 +1,2 @@
 def evaluate(df):
-  return "This is a stub for the evaluation function."
+  return "This is a stub for the evaluation function. The real implementation will compare your predictions with the held out production measurements."
